b0bb54d69e755793311f2888953e882a7be07f7f
sooko_io <42238551+sooko@users.noreply.github.com>
2019-09-11 04:38:42 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='dial',
+      version='0.1',
+      description='Futuristic dial',
+      url='http://github.com/sooko/dashboard',
+      author='sooko',
+      author_email='hansmotor89@gmail.com',
+      license='MIT',
+      packages=['dial'],
+      zip_safe=False)
+      
